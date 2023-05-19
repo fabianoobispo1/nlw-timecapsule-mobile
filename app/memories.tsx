5281e81747850a954fe1c30e0ac2fbc00ec7c69d
fabianoobispo1 <fabianoobispo@icloud.com>
2023-05-19 19:20:47 -0300

    aula 05

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,27 +1,105 @@
-import { Link } from 'expo-router'
-import React from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { Link, useRouter } from 'expo-router'
+import React, { useEffect, useState } from 'react'
+import { ScrollView, TouchableOpacity, View, Text, Image } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Icon from '@expo/vector-icons/Feather'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+
+dayjs.locale(ptBr)
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  id: string
+  createdAt: string
+}
+
 export default function Memories() {
+  const router = useRouter()
+  const [memories, setMemories] = useState<Memory[]>([])
   const { bottom, top } = useSafeAreaInsets()
+
+  async function singOut() {
+    await SecureStore.deleteItemAsync('token')
+    router.push('/')
+  }
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
+
   return (
-    <View
-      className="flex-1 px-8"
-      style={{ paddingBottom: bottom, marginTop: top }}
-      /*    contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
-       */
+    <ScrollView
+      className="flex-1"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="mt-4 flex-row items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-8">
         <NLWLogo />
 
-        <Link href="/new" asChild>
-          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
-            <Icon name="arrow-left" size={16} color="#fff" />
+        <View className="flex-row gap-2">
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+
+          <TouchableOpacity
+            onPress={singOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
           </TouchableOpacity>
-        </Link>
+        </View>
       </View>
-    </View>
+
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className=" font-body text-xs text-gray-50">
+                  {dayjs(memory.createdAt).format('D [ de ]MMMM[, ] YYYY')}
+                </Text>
+              </View>
+
+              <View className="space-y-4 px-8">
+                <Image
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="text-ms font-body text-gray-200">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
+      </View>
+    </ScrollView>
   )
 }
